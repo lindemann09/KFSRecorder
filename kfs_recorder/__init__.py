8a8a2c4b364a1df08652b8da9d7ab0441ca11cab
Oliver Lindemann <lindemann09@users.noreply.github.com>
2026-05-13 15:39:18 +0200

    bugfix

--- kfs_recorder/__init__.py
+++ kfs_recorder/__init__.py
@@ -8,4 +8,3 @@ __author__ = 'Oliver Lindemann <lindemann@essb.eur.nl>'
 
 from ._sensor_serial import ForceSensorSerial
 from ._time_stamped_data import TSData, TSFloat, TSInt
-from ._timer import get_time
